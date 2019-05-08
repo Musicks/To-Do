170b8cbd8cab44e9e907b928ede75861fe9e893c
Sagar Khadka <breakdowns.blasts@gmail.com>
2019-05-08 18:47:28 +0545

    Added edit notes on item click

--- ArchitectureExample/app/src/main/java/com/example/architectureexample/AddEditNoteActivity.java
+++ ArchitectureExample/app/src/main/java/com/example/architectureexample/AddEditNoteActivity.java
@@ -10,7 +10,9 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
+public class AddEditNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_ID =
+            "com.example.architectureexample.EXTRA_ID";
     public static final String EXTRA_TITLE =
             "com.example.architectureexample.EXTRA_TITLE";
     public static final String EXTRA_DESCRIPTION =
@@ -34,7 +36,22 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPickerPriority.setMaxValue(10);
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Note");
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra(EXTRA_ID))
+        {
+            setTitle("Edit Note");
+            editTextTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+            editTextDescription.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            numberPickerPriority.setValue(intent.getIntExtra(EXTRA_PRIORITY,1));
+        }
+        else
+        {
+            setTitle("Add Note");
+        }
+
+
     }
 
     private void saveNote()
@@ -53,6 +70,12 @@ public class AddNoteActivity extends AppCompatActivity {
         data.putExtra(EXTRA_TITLE,title);
         data.putExtra(EXTRA_DESCRIPTION, description);
         data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id !=-1)
+        {
+            data.putExtra(EXTRA_ID, id);
+        }
 
         setResult(RESULT_OK, data);
         finish();
